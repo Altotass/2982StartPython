41a9c1129a1d684316cd5790e9ae50eb7590412d
Altotas <ray1@mail.ru>
2022-11-20 12:46:04 +0300

    hw7 done

---
+++ HW7/Main.py
@@ -0,0 +1,8 @@
+# Задание в группах: Создать телефонный справочник с возможностью импорта и 
+# экспорта данных в нескольких форматах.
+
+from File_writing import writing_scv
+from File_writing import writing_txt
+
+writing_scv()
+writing_txt()
